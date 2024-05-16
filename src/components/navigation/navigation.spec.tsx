9b1e394ab712650df56b4e5851dadfd0f7bca038
arielferen <arieliten85@gmail.com>
2024-05-16 13:14:43 -0300

    test:fix error test test was stopped

--- src/components/navigation/navigation.spec.tsx
+++ src/components/navigation/navigation.spec.tsx
@@ -1,26 +1,21 @@
 describe("Navogation", () => {
   beforeEach(() => {
+    cy.viewport(1280, 720);
     cy.visit("http://localhost:5173");
   });
   it("Navbar should render correctly", () => {
-    cy.viewport(1280, 720);
     cy.wait(2000);
     cy.get('[data-test="header"]').should("be.visible");
   });
 
-  describe("Navigation", () => {
+  describe("Navigation to register and login pages correctly", () => {
     it("should navigate to Register page", () => {
-      cy.viewport(1280, 720);
       cy.get('[data-test="link-register"]').should("be.visible");
       cy.get('[data-test="link-register"]').click();
       cy.wait(2000);
       cy.url().should("include", "/register");
-    });
-  });
 
-  describe("Navigation", () => {
-    it("should navigate to Login page", () => {
-      cy.viewport(1280, 720);
+      cy.wait(2000);
       cy.get('[data-test="link-login"]').should("be.visible");
       cy.get('[data-test="link-login"]').click();
       cy.wait(2000);
