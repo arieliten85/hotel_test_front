55a109716b128f8566ad24e92a77edc8e887f863
Ariel Ferencak <88351452+arieliten85@users.noreply.github.com>
2024-05-16 13:22:05 -0300

    Update card.spec.tsx

--- src/components/card/card.spec.tsx
+++ src/components/card/card.spec.tsx
@@ -3,27 +3,18 @@ describe("Navigation", () => {
      cy.viewport(1280, 720);
     cy.visit("http://localhost:5173");
   });
-
   it("Card should render correctly", () => {
-   
-
     cy.get('[data-test="card"]').should("be.visible");
   });
-
   it("should generate the code correctly", () => {
-   
     cy.get('[data-test="button-generate-code"]').should("be.visible");
-
     cy.get('[data-test="button-generate-code"]:first').click({
       multiple: true,
     });
     cy.get(".Toastify__toast").should("be.visible");
   });
-
   it("it should go to my codes", () => {
-   
     cy.get('[data-test="card__body"]').should("be.visible");
-
     cy.get('[data-test="card__body"]:first').click({
       multiple: true,
     });
