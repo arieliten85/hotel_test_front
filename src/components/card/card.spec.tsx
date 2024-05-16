f5538d130dd3e171648433b56d9cc1d6813bb039
Ariel Ferencak <88351452+arieliten85@users.noreply.github.com>
2024-05-16 13:21:28 -0300

    Update card.spec.tsx

--- src/components/card/card.spec.tsx
+++ src/components/card/card.spec.tsx
@@ -1,16 +1,17 @@
 describe("Navigation", () => {
   beforeEach(() => {
+     cy.viewport(1280, 720);
     cy.visit("http://localhost:5173");
   });
 
   it("Card should render correctly", () => {
-    cy.viewport(1280, 720);
+   
 
     cy.get('[data-test="card"]').should("be.visible");
   });
 
   it("should generate the code correctly", () => {
-    cy.viewport(1280, 720);
+   
     cy.get('[data-test="button-generate-code"]').should("be.visible");
 
     cy.get('[data-test="button-generate-code"]:first').click({
@@ -20,7 +21,7 @@ describe("Navigation", () => {
   });
 
   it("it should go to my codes", () => {
-    cy.viewport(1280, 720);
+   
     cy.get('[data-test="card__body"]').should("be.visible");
 
     cy.get('[data-test="card__body"]:first').click({
